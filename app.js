34155a6b4f21ca4f509dfe5748d3301954856874
francisedulab <francis@edulab.in>
2022-09-30 17:09:33 +0530

    changed schema

--- app.js
+++ app.js
@@ -3,10 +3,12 @@ const mongoose = require('mongoose');
 async function connect(){
 
     await mongoose.connect('mongodb://localhost:27017/interns');
-    const Cat = mongoose.model('Cat', { name: String });
+    const Cat = mongoose.model('Cat', { name: String , color : String});
 
-    const kitty = new Cat({ name: 'peppa' });
-    kitty.save().then(() => console.log('meow'));
+    const peppa = new Cat({ name: 'peppa' });
+    const kitty = new Cat({ name: 'kitty' ,color: 'black' });
+    peppa.save().then(() => console.log('peppa Created'));
+    kitty.save().then(() => console.log('kitty Created'));
 }
 
 connect();
